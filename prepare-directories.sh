b1d0dfb4c34f6c4e9f5ba913e4ee129010310857
Scott Cranton <scott.cranton@solo.io>
2019-07-22 13:23:48 -0400

    updates

--- prepare-directories.sh
+++ prepare-directories.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./data/artifact/artifacts/gloo-system
 mkdir -p ./data/config/{gateways,proxies,upstreams,upstreamgroups}/gloo-system
-mkdir -p ./data/secret/secrets/gloo-system
+mkdir -p ./data/secret/secrets/{default,gloo-system}
