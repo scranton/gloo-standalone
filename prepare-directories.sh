f41acb700c2176c994084a4e791e2b71aa5c520c
Scott Cranton <scott.cranton@solo.io>
2019-07-22 11:42:07 -0400

    initial commit

---
+++ prepare-directories.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p ./data/config/{upstreams,upstreamgroups,virtualservices,proxies,gateways}/gloo-system
+mkdir -p ./data/artifact/artifacts/gloo-system
+mkdir -p ./data/secret/secrets/{gloo-system,default}
